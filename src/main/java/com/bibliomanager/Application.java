767c02c4165128d61a8be25203739b3f82574905
SteveAnderson95 <steve.anderson.hakiz25@gmail.com>
2026-03-05 19:05:57 +0000

    feat : implement login user(student) interface

--- src/main/java/com/bibliomanager/Application.java
+++ src/main/java/com/bibliomanager/Application.java
@@ -1,17 +1,35 @@
 package com.bibliomanager;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+
+    private double x = 0;
+    private double y = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = fxmlLoader.load();
+
+        root.setOnMousePressed((MouseEvent e) -> {
+            x = e.getSceneX();
+            y = e.getSceneY();
+        });
+        root.setOnMouseDragged((MouseEvent e) -> {
+            stage.setX(e.getSceneX() - x);
+            stage.setY(e.getSceneY() - y);
+        });
+
+        Scene scene = new Scene(root);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/bibliomanager/Controller.java
+++ src/main/java/com/bibliomanager/Controller.java
@@ -1,14 +1,34 @@
 package com.bibliomanager;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
+    private Button minimize;
+    @FXML
+    private Button loginBtn;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private AnchorPane root;
+
+    @FXML
+    private TextField studentPassword;
+
+    @FXML
+    private TextField studentUsername;
+
+    @FXML
+    public void exit () {
+        System.exit(0);
     }
+    @FXML
+    private void minimize () {
+        Stage stage = (Stage) minimize.getScene().getWindow();
+        stage.setIconified(true);
+    };
 }
