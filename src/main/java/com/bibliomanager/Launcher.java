2bf94d1f80fa402459784404c94b4159560d1cd9
SteveAnderson95 <steve.anderson.hakiz25@gmail.com>
2026-04-07 02:04:47 +0100

    chore : reset project for clean rebuild

--- src/main/java/com/bibliomanager/Launcher.java
+++
@@ -1,7 +0,0 @@
-package com.bibliomanager;
-
-public class Launcher {
-    public static void main(String[] args) {
-        javafx.application.Application.launch(Application.class, args);
-    }
-}
