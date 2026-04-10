187092ccc26dadb169abbdb9c40ae7c89eeb8c1f
SteveAnderson95 <steve.anderson.hakiz25@gmail.com>
2026-04-10 23:57:45 +0100

    feat(utils): add date formatting and comparison helpers for library loans

--- src/main/java/com/bibliomanager/utils/DateUtils.java
+++ src/main/java/com/bibliomanager/utils/DateUtils.java
@@ -1,4 +1,15 @@
 package com.bibliomanager.utils;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DateUtils {
+
+    public static String getFormattedDate() {
+        LocalDate now = java.time.LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE d MMMM yyyy");
+        String date = now.format(formatter);
+        return date.substring(0, 1).toUpperCase() + date.substring(1);
+    }
+
 }
