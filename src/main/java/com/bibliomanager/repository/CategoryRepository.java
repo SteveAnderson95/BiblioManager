dd6a55f990fec97ad1a0a5aa43617c7259780878
SteveAnderson95 <steve.anderson.hakiz25@gmail.com>
2026-04-10 23:54:21 +0100

    feat(repo): add category counting functionality

--- src/main/java/com/bibliomanager/repository/CategoryRepository.java
+++ src/main/java/com/bibliomanager/repository/CategoryRepository.java
@@ -1,4 +1,26 @@
 package com.bibliomanager.repository;
 
+import com.bibliomanager.utils.DatabaseManager;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CategoryRepository {
+
+    public int getTotalCategoriesCount() {
+        String sql = "SELECT COUNT(*) FROM categories";
+        try (
+                Connection connection = DatabaseManager.getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql)
+                ) {
+            if (resultSet.next())
+                return resultSet.getInt(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
