429ae9ce1d00ca3658197e6bebf20078e06e821a
SteveAnderson95 <steve.anderson.hakiz25@gmail.com>
2026-04-27 05:27:54 +0100

    feat : add AppSettings dao (model) & Database update by creating app_settings table

--- src/main/java/com/bibliomanager/model/AppSettings.java
+++ src/main/java/com/bibliomanager/model/AppSettings.java
@@ -1,4 +1,24 @@
 package com.bibliomanager.model;
 
 public class AppSettings {
+    private String libraryName;
+    private String libraryAddress;
+    private int defaultLoanDuration;
+    private int maxLoansPerStudent;
+
+    public AppSettings(String libraryName, String libraryAddress, int defaultLoanDuration, int maxLoansPerStudent) {
+        this.libraryName = libraryName;
+        this.libraryAddress = libraryAddress;
+        this.defaultLoanDuration = defaultLoanDuration;
+        this.maxLoansPerStudent = maxLoansPerStudent;
+    }
+
+    public String getLibraryName() { return libraryName; }
+    public String getLibraryAddress() { return libraryAddress; }
+    public int getDefaultLoanDuration() { return defaultLoanDuration; }
+    public int getMaxLoansPerStudent() { return maxLoansPerStudent; }
+    public void setLibraryName(String v) { this.libraryName = v; }
+    public void setLibraryAddress(String v) { this.libraryAddress = v; }
+    public void setDefaultLoanDuration(int v) { this.defaultLoanDuration = v; }
+    public void setMaxLoansPerStudent(int v) { this.maxLoansPerStudent = v; }
 }
